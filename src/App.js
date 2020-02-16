7cdee7ec4f697e6004c5a238e9d3db4f70e62a1e
cross000 <39590588+cross000@users.noreply.github.com>
2020-02-16 21:27:24 +1000

    extras

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink
+} from "react-router-dom";
 import "./App.css";
 
 function App() {
@@ -37,15 +42,28 @@ function App() {
           <nav>
             <div className="nav">
               <div className="line" />
-              <Link to="/about" className="link">
+
+              <NavLink
+                to="/about"
+                className="link"
+                activeStyle={{ color: "blue" }}
+              >
                 About
-              </Link>
-              <Link to="/" className="link">
+              </NavLink>
+              <NavLink
+                activeStyle={{ color: "blue" }}
+                to="/home"
+                className="link"
+              >
                 Home
-              </Link>
-              <Link to="/projects" className="link">
+              </NavLink>
+              <NavLink
+                to="/projects"
+                className="link"
+                activeStyle={{ color: "blue" }}
+              >
                 Projects
-              </Link>
+              </NavLink>
 
               <div className="line" />
             </div>
@@ -58,32 +76,34 @@ function App() {
             <Route path="/projects">
               <Users />
             </Route>
-            <Route path="/">
+            <Route path="/home">
               <Home />
             </Route>
           </Switch>
         </div>
       </Router>
-      <footer id="footer">Contact 0490373900</footer>
+      <footer id="footer"></footer>
     </>
   );
 }
 function Home() {
   return (
     <>
-      <div className="texty">
-        <h2>Hi there...</h2>
-        <p>
-          I am a Computer Science Student at QUT. <br /> I am currently in my
-          final year and enjoy web development, cloud computing and data
-          science.
-        </p>
+      <div className="all">
+        <div className="texty">
+          <h2>Hi there...</h2>
+          <p>
+            I am a Computer Science Student at QUT. <br /> I am currently in my
+            final year and enjoy web development, cloud computing and data
+            science.
+          </p>
+        </div>
+        <img
+          className="img1"
+          src="https://www.pngkey.com/png/detail/432-4323109_sri-yantra-gold-geometric-shapes-png.png"
+          alt="shape"
+        />
       </div>
-      <img
-        className="img1"
-        src="https://www.pngkey.com/png/detail/432-4323109_sri-yantra-gold-geometric-shapes-png.png"
-        alt="shape"
-      />
     </>
   );
 }
@@ -91,112 +111,156 @@ function Home() {
 function About() {
   return (
     <>
-      <div className="texty">
-        <h2>Experience</h2>Interned at Local Government Association of
-        Queensland in their LG Sherlock department.
-        <br />
-        Helped create work flowsin alteryx, visualize data through tableau,
-        python scripts and learnt about modern data management on a large scale
-        <br />
-        <h2>Education</h2>Bachelor of IT (Computer Science) @ QUT third year
-        <br />
-        <p>
-          <span style={{ fontSize: "1.2em" }}>GPA:</span> 6.6
-        </p>
-        <p>
-          Relevant coursework: Cloud Computing, Web Computing, Software
-          Development, Databases, Statistics, Project Management
-        </p>
-        <br />
-        <h2>Skills</h2>
-      </div>
-      <div className="img3">
-        <figure>
-          <img
-            id="js"
-            style={{ paddingLeft: "1em", paddingRight: "1em", width: "5em" }}
-            src="https://unpkg.com/simple-icons@2.4.0/icons/javascript.svg"
-            alt="JavaScipt"
-          />
-          <figcaption style={{ textAlign: "center" }}>JavaScipt</figcaption>
-        </figure>
+      <div className="all">
+        <div className="texty">
+          <h2>Experience</h2>Interned at Local Government Association of
+          Queensland in their LG Sherlock department.
+          <br />
+          Helped create work flowsin alteryx, visualize data through tableau,
+          python scripts and learnt about modern data management on a large
+          scale
+          <br />
+          <h2>Education</h2>Bachelor of IT (Computer Science) @ QUT third year
+          <br />
+          <p>
+            <span style={{ fontSize: "1.2em" }}>GPA:</span> 6.6
+          </p>
+          <p>
+            Relevant coursework: Cloud Computing, Web Computing, Software
+            Development, Databases, Statistics, Project Management
+          </p>
+          <br />
+          <h2>Skills</h2>
+        </div>
+        <div className="img3">
+          <figure>
+            <img
+              id="js"
+              style={{ paddingLeft: "1em", paddingRight: "1em", width: "5em" }}
+              src="https://unpkg.com/simple-icons@2.4.0/icons/javascript.svg"
+              alt="JavaScipt"
+            />
+            <figcaption style={{ textAlign: "center" }}>JavaScipt</figcaption>
+          </figure>
 
-        <figure>
-          <img
-            id="React"
-            style={{ paddingLeft: "1em", paddingRight: "1em", width: "5em" }}
-            src="https://unpkg.com/simple-icons@2.4.0/icons/react.svg"
-            alt="React"
-          />{" "}
-          <figcaption style={{ textAlign: "center" }}>React</figcaption>
-        </figure>
+          <figure>
+            <img
+              id="React"
+              style={{ paddingLeft: "1em", paddingRight: "1em", width: "5em" }}
+              src="https://unpkg.com/simple-icons@2.4.0/icons/react.svg"
+              alt="React"
+            />{" "}
+            <figcaption style={{ textAlign: "center" }}>React</figcaption>
+          </figure>
 
-        <figure>
-          <img
-            id="Python"
-            style={{ paddingLeft: "1em", paddingRight: "1em", width: "5em" }}
-            src="https://unpkg.com/simple-icons@2.4.0/icons/python.svg"
-            alt="Python"
-          />
-          <figcaption style={{ textAlign: "center" }}>Python</figcaption>
-        </figure>
+          <figure>
+            <img
+              id="Python"
+              style={{ paddingLeft: "1em", paddingRight: "1em", width: "5em" }}
+              src="https://unpkg.com/simple-icons@2.4.0/icons/python.svg"
+              alt="Python"
+            />
+            <figcaption style={{ textAlign: "center" }}>Python</figcaption>
+          </figure>
 
-        <figure>
-          <img
-            id="Docker"
-            style={{ paddingLeft: "1em", paddingRight: "1em", width: "5em" }}
-            src="https://unpkg.com/simple-icons@2.4.0/icons/docker.svg"
-            alt="Docker"
-          />
-          <figcaption style={{ textAlign: "center" }}>Docker</figcaption>
-        </figure>
+          <figure>
+            <img
+              id="Docker"
+              style={{ paddingLeft: "1em", paddingRight: "1em", width: "5em" }}
+              src="https://unpkg.com/simple-icons@2.4.0/icons/docker.svg"
+              alt="Docker"
+            />
+            <figcaption style={{ textAlign: "center" }}>Docker</figcaption>
+          </figure>
 
-        <figure>
-          <img
-            id="aws"
-            style={{ paddingLeft: "1em", paddingRight: "1em", width: "5em" }}
-            src="https://unpkg.com/simple-icons@2.4.0/icons/amazonaws.svg"
-            alt="aws"
-          />
-          <figcaption style={{ textAlign: "center" }}>AmazonAWS</figcaption>
-        </figure>
-        <figure>
-          <img
-            id="git"
-            style={{ paddingLeft: "1em", paddingRight: "1em", width: "5em" }}
-            src="https://unpkg.com/simple-icons@2.4.0/icons/git.svg"
-            alt=".net"
-          />
-          <figcaption style={{ textAlign: "center" }}>Git</figcaption>
-        </figure>
-        <figure>
-          <img
-            id="java"
-            style={{ paddingLeft: "1em", paddingRight: "1em", width: "5em" }}
-            src="https://unpkg.com/simple-icons@2.4.0/icons/java.svg"
-            alt="java"
-          />
-          <figcaption style={{ textAlign: "center" }}>Java</figcaption>
-        </figure>
+          <figure>
+            <img
+              id="aws"
+              style={{ paddingLeft: "1em", paddingRight: "1em", width: "5em" }}
+              src="https://unpkg.com/simple-icons@2.4.0/icons/amazonaws.svg"
+              alt="aws"
+            />
+            <figcaption style={{ textAlign: "center" }}>AmazonAWS</figcaption>
+          </figure>
+          <figure>
+            <img
+              id="git"
+              style={{ paddingLeft: "1em", paddingRight: "1em", width: "5em" }}
+              src="https://unpkg.com/simple-icons@2.4.0/icons/git.svg"
+              alt=".net"
+            />
+            <figcaption style={{ textAlign: "center" }}>Git</figcaption>
+          </figure>
+          <figure>
+            <img
+              id="SQL"
+              style={{ paddingLeft: "1em", paddingRight: "1em", width: "5em" }}
+              src="https://unpkg.com/simple-icons@2.4.0/icons/mysql.svg"
+              alt="sql"
+            />
+            <figcaption style={{ textAlign: "center" }}>MySQL</figcaption>
+          </figure>
+          <figure>
+            <img
+              id="java"
+              style={{ paddingLeft: "1em", paddingRight: "1em", width: "5em" }}
+              src="https://unpkg.com/simple-icons@2.4.0/icons/java.svg"
+              alt="java"
+            />
+            <figcaption style={{ textAlign: "center" }}>Java</figcaption>
+          </figure>
 
-        <figure>
-          <img
-            id="tableau"
-            style={{ paddingLeft: "1em", paddingRight: "1em", width: "5em" }}
-            src="https://unpkg.com/simple-icons@2.4.0/icons/tableau.svg"
-            alt="tableau"
-          />
-          <figcaption style={{ textAlign: "center" }}>
-            Tableau and Alteryx
-          </figcaption>
-        </figure>
+          <figure>
+            <img
+              id="tableau"
+              style={{ paddingLeft: "1em", paddingRight: "1em", width: "5em" }}
+              src="https://unpkg.com/simple-icons@2.4.0/icons/tableau.svg"
+              alt="tableau"
+            />
+            <figcaption style={{ textAlign: "center" }}>
+              Tableau and Alteryx
+            </figcaption>
+          </figure>
+        </div>
       </div>
     </>
   );
 }
 
 function Users() {
-  return <h2>Users</h2>;
+  return (
+    <>
+      <div className="all">
+        <div className="main">
+          <div className="gallery">
+            <a href="https://github.com/Callum-Ross/cab432-assignment1">
+              <img
+                src="https://i.imgur.com/52Z7QOx.png"
+                alt="Cinque Terre"
+                width="600"
+                height="400"
+              />
+            </a>
+            <div className="desc">
+              <h1>Points of interest/weather API Mashup</h1>
+              <br />
+              Users would select a location on the map and be shown 10 POI's
+              nearby.
+              <br /> They could then find the weather forecast for each location
+              by clicking on the pin on the map.
+            </div>
+          </div>
+
+          <div class="gallery">
+            <a target="_blank" href="img_forest.jpg">
+              <img src="img_forest.jpg" alt="Forest" width="600" height="400" />
+            </a>
+            <div class="desc">Add a description of the image here</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
